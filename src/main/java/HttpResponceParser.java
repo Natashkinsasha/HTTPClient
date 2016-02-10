77eb96aeb8d9c1bbdf0298e9708ce1e5ce4f84e1
Natashkin Alex <natashkinsasha@gmail.com>
2016-02-10 22:16:04 +0300

    Доделал парсер

--- src/main/java/HttpResponceParser.java
+++ src/main/java/HttpResponceParser.java
@@ -5,7 +5,11 @@ import java.util.regex.Pattern;
 public class HttpResponceParser {
 
 
-    public HttpResponce parse(String responc) throws ResponceException {
+    public HttpResponce parse(String responce) throws ResponceException {
+        String[] r = responce.split("\r\n\r\n");
+        String sLineAndHeader = r[0];
+        String body = r[1];
+        body = body.substring(8);
         String startLineString = null;
         Headers headers = new Headers();
         double version = 0;
@@ -14,7 +18,7 @@ public class HttpResponceParser {
         String header;
         StringBuilder builderHeader = null;
         Pattern p = Pattern.compile("HTTP.*");
-        Matcher m = p.matcher(responc);
+        Matcher m = p.matcher(sLineAndHeader);
         if (m.find()) {
             startLineString = m.group();
         } else {
@@ -34,14 +38,33 @@ public class HttpResponceParser {
         } else {
             throw new ResponceException();
         }
+
+        p = Pattern.compile("\\s[^\\d]+");
+        m = p.matcher(startLineString);
+        if (m.find()) {
+            reasonPhrase = m.group();
+        } else {
+            throw new ResponceException();
+        }
         p = Pattern.compile(".+:.+");
-        m = p.matcher(responc);
+        m = p.matcher(sLineAndHeader);
         while (m.find()) {
             header = m.group();
             StringTokenizer t = new StringTokenizer(header, ": ");
             headers.addHeader(t.nextToken(), t.nextToken());
         }
-        return new HttpResponce(new ResponceStartingLine(version, statusCode, reasonPhrase), headers);
+        return new HttpResponce(new ResponceStartingLine(version, statusCode, reasonPhrase), headers, new MessageBody(body));
+    }
+
+    public static void main(String argv[]) throws Exception {
+        HttpResponceParser parser = new HttpResponceParser();
+        parser.parse("HTTP\\1.1 400 ddfgf egh \n" +
+                "Start: fghg\n" +
+                "Begin: df ,df fd g6 \n" +
+                "\n" +
+                "dsfdgf \n" +
+                "gh \n" +
+                "yrtjuyytuy  yt");
     }
 
 }
